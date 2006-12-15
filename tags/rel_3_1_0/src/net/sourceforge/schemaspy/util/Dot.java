8fa5a104c504e72fc3f50697b283e1d042edf605
johncurrier <johncurrier@e86a8a9b-1e1f-0410-8769-81452b7583c3>
2006-12-15 23:11:15 +0000

    Bug 1571711 - can't detect dot version on MacOS-X (applied to revision 3.1.0)

--- tags/rel_3_1_0/src/net/sourceforge/schemaspy/util/Dot.java
+++ tags/rel_3_1_0/src/net/sourceforge/schemaspy/util/Dot.java
@@ -1,7 +1,7 @@
 package net.sourceforge.schemaspy.util;
 
 import java.io.*;
-import java.util.*;
+import java.util.regex.*;
 
 public class Dot {
     private static Dot instance = new Dot();
@@ -10,16 +10,21 @@ public class Dot {
     private final Version badVersion = new Version("2.4");
 
     private Dot() {
-        String tempVersion = null;
+        String versionText = null;
         try {
+            // dot -V should return something similar to:
+            //  dot version 2.8 (Fri Feb  3 22:38:53 UTC 2006)
+            // or sometimes something like:
+            //  dot - Graphviz version 2.9.20061004.0440 (Wed Oct 4 21:01:52 GMT 2006)
             String dotCommand = "dot -V";
             Process process = Runtime.getRuntime().exec(dotCommand);
             BufferedReader reader = new BufferedReader(new InputStreamReader(process.getErrorStream()));
             String versionLine = reader.readLine();
-            StringTokenizer tokenizer = new StringTokenizer(versionLine);
-            if (tokenizer.nextToken().equals("dot")) {  // skip 'dot'
-                tokenizer.nextToken();                  // skip 'version'
-                tempVersion = tokenizer.nextToken();    // x.x.x
+            
+            // look for a number followed numbers or dots
+            Matcher matcher = Pattern.compile("[0-9][0-9.]+").matcher(versionLine);
+            if (matcher.find()) {
+                versionText = matcher.group();
             } else {
                 System.err.println();
                 System.err.println("Invalid dot configuration detected.  '" + dotCommand + "' returned:");
@@ -28,7 +33,7 @@ public class Dot {
         } catch (Exception validDotDoesntExist) {
         }
 
-        version = new Version(tempVersion);
+        version = new Version(versionText);
     }
 
     public static Dot getInstance() {
@@ -116,9 +121,9 @@ public class Dot {
     }
 
     public class DotFailure extends IOException {
-		private static final long serialVersionUID = 3833743270181351987L;
+        private static final long serialVersionUID = 3833743270181351987L;
 
-		public DotFailure(String msg) {
+        public DotFailure(String msg) {
             super(msg);
         }
     }
